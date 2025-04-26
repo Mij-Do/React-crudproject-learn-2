7ed68869bf515e215e6b84189318f22fd36c1e9b
ahmed <ahmedmijdo2@gmail.com>
2025-04-26 14:17:32 +0300

    product card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import './App.css'
+import ProductsCard from "./components/ProductsCard"
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl underline text-indigo-300'>Hello World</h1>
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-2 grid-cols-1 ">
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+      </div>
     </>
   )
 }
---
+++ src/components/ProductsCard.tsx
@@ -0,0 +1,40 @@
+interface IProps {
+
+}
+
+const ProductsCard = ({}: IProps) => {
+    return (
+        <>
+            <div className="flex flex-col border border-gray-200 rounded-md p-2">
+                <img 
+                    className="rounded-md"
+                    src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" 
+                    alt="Green Image" 
+                />
+                <h2>Green Image</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, tempora!</p>
+
+                <div className="flex space-x-2 my-5">
+                    <span className="w-5 h-5 bg-indigo-500 rounded-full cursor-pointer" />
+                    <span className="w-5 h-5 bg-black rounded-full cursor-pointer" />
+                    <span className="w-5 h-5 bg-red-500 rounded-full cursor-pointer" />
+                </div>
+
+                <div className="flex items-center justify-between">
+                    <span>$500,000</span>
+                    <img 
+                        className="rounded-full w-10 h-10"
+                        src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" 
+                        alt="Green Image" 
+                    />
+                </div>
+                <div className="mt-5 flex space-x-2">
+                    <button className="w-full p-2 text-white rounded-md cursor-pointer bg-indigo-600">EDIT</button>
+                    <button className="w-full p-2 text-white rounded-md cursor-pointerinter bg-red-600">DELETE</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProductsCard;
