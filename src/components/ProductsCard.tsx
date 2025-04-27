7835aafcd992686643feff09e64b6eac66e59f0a
ahmed <ahmedmijdo2@gmail.com>
2025-04-27 15:45:42 +0300

    add Image component

--- src/components/ProductsCard.tsx
+++ src/components/ProductsCard.tsx
@@ -1,3 +1,5 @@
+import Image from "./Image";
+
 interface IProps {
 
 }
@@ -6,10 +8,9 @@ const ProductsCard = ({}: IProps) => {
     return (
         <>
             <div className="flex flex-col border border-gray-200 rounded-md p-2">
-                <img 
-                    className="rounded-md"
-                    src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" 
-                    alt="Green Image" 
+                <Image imageUrl="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"
+                        className="rounded-md"
+                        alt="Green Image"
                 />
                 <h2>Green Image</h2>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, tempora!</p>
@@ -22,10 +23,9 @@ const ProductsCard = ({}: IProps) => {
 
                 <div className="flex items-center justify-between">
                     <span>$500,000</span>
-                    <img 
-                        className="rounded-full w-10 h-10"
-                        src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" 
-                        alt="Green Image" 
+                    <Image imageUrl="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"
+                        className="rounded-full w-10 h-10 object-contain"
+                        alt="Green Image"
                     />
                 </div>
                 <div className="mt-5 flex space-x-2">
